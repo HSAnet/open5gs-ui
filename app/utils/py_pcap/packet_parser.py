c06b5b4f114c47460adc23f3cca620b891728671
Winkler-Jonas <jonas.eduard.winkler@gmail.com>
2023-12-23 13:42:15 +0100

    Added ipv6 parser - only testet with icmp

--- app/utils/py_pcap/packet_parser.py
+++ app/utils/py_pcap/packet_parser.py
@@ -1,13 +1,13 @@
-import ipaddress
+import socket
 import struct
+import ipaddress
+import numpy as np
 from configparser import ParsingError
 from typing import Callable, Tuple, Dict, List
-import numpy as np
-import socket
 
 bytes_to_mac: Callable[[bytes], str] = lambda b: ':'.join(map('{:02x}'.format, b))
 bytes_to_ip: Callable[[bytes], str] = lambda b: str(ipaddress.ip_address(b))
-
+parse_ports: Callable[[bytes], tuple] = lambda b: struct.unpack_from(bld_str_fmt([16, 16]), b)
 
 string_format: Dict[int, str] = {
     1: '{}s',
@@ -52,6 +52,7 @@ def bld_str_fmt(bit_list: List[int]) -> str:
 def __parse_ethernet_frame(data: bytes) -> Tuple[str, str, int]:
     """
     Function parses provided data into ethernet frame (src- a. dst MAC + Ethertype)
+
     EtherType:
         - IPv4: 0x0800 -> 2048
         - IPv6: 0x86DD -> 34525
@@ -89,15 +90,26 @@ def __parse_ip_packet(data: bytes) -> tuple:
         # The & operation retrieves that value. It represents the amount of words (32-bits) the header uses.
         # 15 is dec for 0xf but doesn't need to be converted
         ip_header_len = ((v_ihl & 15) * 32) // 8
-        src_port, dst_port = struct.unpack_from(bld_str_fmt([16, 16]), data[ip_header_len:])
-        return src_ip, src_port, dst_ip, dst_port, protocol_numbers.get(prot, '')
+        src_port, dst_port = parse_ports(data[ip_header_len:])
+        return bytes_to_ip(src_ip), src_port, bytes_to_ip(dst_ip), dst_port, protocol_numbers.get(prot, '')
     except struct.error:
         raise ParsingError("Provided data too short for IPv4 packet!")
 
 
 def __parse_ipv6_packet(data: bytes) -> tuple:
-    #print('inside_IP6')
-    pass
+    """
+    Function parses provided data into Ipv6 packet format
+
+    IPv6-Packet-Architecture:
+        Version-4, Traffic-Class-8, Flow-Label-20, Payload-Length-16,
+        Next-Header-8, Hop-Limit-8, Source-Address-128, Dest-Address-128
+
+    :param data: bytes data to be parsed
+    :return: Tuple(Source_IP, Source_Port, Dest_IP, Dest_Port)
+    """
+    vtfl, payload_len, nxt_head, hop_lmt, src_ip, dst_ip = struct.unpack_from(bld_str_fmt([32, 16, 8, 8, 128, 128]), data)
+    src_port, dst_port = parse_ports(data[40:])
+    return bytes_to_ip(src_ip), src_port, bytes_to_ip(dst_ip), dst_port
 
 
 def __parse_arp_packet(data: bytes) -> Tuple[int, int, str, str]:
@@ -133,14 +145,10 @@ def parse_packet(packet_data: bytes) -> np.array:
     try:
         dst_mac, src_mac, eth_type = __parse_ethernet_frame(packet_data)
         parsed_packet = globals()[f'__parse_{[var.rsplit('_')[-1].lower() for var in dir(socket) if 'ETHERTYPE' in var and eth_type == getattr(socket, var)][0]}_packet'](packet_data[14:])
+    except struct.error:
+        # Error while parsing - ignore packet - continue
+        pass
     except ParsingError:
         print('Error parsing ethernet frame')
     except IndexError:
         print('EtherType not found!')
-
-
-if __name__ == '__main__':
-    struct.unpack_from('>HHHHHHH', b'E\x00\x00')
-    # ether_type: int = 2048
-    # fun = [var.rsplit('_')[-1].lower() for var in dir(socket) if 'ETHERTYPE' in var and ether_type == getattr(socket, var)][0]
-    # locals()[f'__parse_{fun}_packet'](b'xy')
