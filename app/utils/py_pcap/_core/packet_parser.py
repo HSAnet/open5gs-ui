a538c671b64180545db83e8fcdd262412fbf564e
Winkler-Jonas <jonas.eduard.winkler@gmail.com>
2023-12-27 10:21:37 +0100

    Finished packet parser

--- app/utils/py_pcap/_core/packet_parser.py
+++ app/utils/py_pcap/_core/packet_parser.py
@@ -1,23 +1,22 @@
 import socket
 import struct
 import ipaddress
-from exceptions import CaptureError
+
+from .._utils import Packet
 
 import numpy as np
 from typing import Callable, Dict, List, Union
 
-
+# string format for mac address
 bytes_to_mac: Callable[[bytes], str] = lambda b: ':'.join(map('{:02x}'.format, b))
+# parse bytes to ipv4/6 address
 bytes_to_ip: Callable[[bytes], str] = lambda b: str(ipaddress.ip_address(b))
+# parse two 16bit Integers to python Ints
 parse_ports: Callable[[bytes], tuple] = lambda b: struct.unpack_from(bld_str_fmt([16, 16]), b)
-
-
+# Store supported EtherTypes (from socket module) in dict
 socket_eth_types: Dict[str, int] = {key: value for (key, value) in socket.__dict__.items() if 'ETHERTYPE' in key}
 
-
-PKG_COLUMNS: List[str] = ['TimeStamp', 'EtherType', 'Direction', 'SrcMac', 'DstMac', 'SrcAddress', 'SrcPort', 'DstAddress', 'DstPort', 'ProtType', 'Operation', 'Size']
-
-
+# Bits to struct.string_format dict
 string_format: Dict[int, str] = {
     1: '{}s',
     8: 'B',
@@ -27,34 +26,13 @@ string_format: Dict[int, str] = {
 }
 
 
-protocol_numbers = {
-    0: "Reserved",
-    1: "ICMP",
-    2: "IGMP",
-    3: "GGP",
-    4: "IP-in-IP",
-    5: "IPSec AH",
-    6: "IPSec ESP",
-    7: "IPCP",
-    8: "OSPF",
-    9: "BGP",
-    10: "PING",
-    11: "Traceroute",
-    12: "DCCP",
-    13: "Mobile IP",
-    17: "UDP",
-    20: "FTP-C",
-    21: "FTP-D",
-    22: "SSH",
-    25: "SMTP",
-    53: "DNS",
-    69: "TFTP",
-    123: "NTP",
-    443: "HTTPS"
-}
-
-
 def bld_str_fmt(bit_list: List[int]) -> str:
+    """
+    Function parses list of bit representation into struct.string_format (little endian)
+
+    :param bit_list: List of bit values
+    :return: struct.string_format (little endian)
+    """
     return '>' + ''.join([string_format[1].format(bits // 8) if bits not in string_format.keys() else string_format[bits] for bits in bit_list])
 
 
@@ -72,9 +50,9 @@ def __parse_ethernet_frame(data: bytes, ref: np.array) -> None:
     :return: dst-MAC (str), src-MAC (str), EtherType (int)
     """
     dst_mac, src_mac, eth_type = struct.unpack_from('>6s6sH', data)
-    ref[PKG_COLUMNS.index('EtherType')] = eth_type
-    ref[PKG_COLUMNS.index('SrcMac')] = bytes_to_mac(src_mac)
-    ref[PKG_COLUMNS.index('DstMac')] = bytes_to_mac(dst_mac)
+    ref[Packet.ETHERTYPE.index] = eth_type
+    ref[Packet.SOURCE_MAC.index] = bytes_to_mac(src_mac)
+    ref[Packet.DESTINATION_MAC.index] = bytes_to_mac(dst_mac)
 
 
 def __parse_ip_packet(data: bytes, ref: np.array) -> None:
@@ -96,10 +74,10 @@ def __parse_ip_packet(data: bytes, ref: np.array) -> None:
     # 15 is dec for 0xf but doesn't need to be converted
     ip_header_len = ((v_ihl & 15) * 32) // 8
     src_port, dst_port = parse_ports(data[ip_header_len:])
-    ref[PKG_COLUMNS.index('SrcAddress')] = bytes_to_ip(src_ip)
-    ref[PKG_COLUMNS.index('SrcPort')] = src_port
-    ref[PKG_COLUMNS.index('DstAddress')] = bytes_to_ip(dst_ip)
-    ref[PKG_COLUMNS.index('DstPort')] = dst_port
+    ref[Packet.SOURCE_IP.index] = bytes_to_ip(src_ip)
+    ref[Packet.SOURCE_PORT.index] = src_port
+    ref[Packet.DESTINATION_IP.index] = bytes_to_ip(dst_ip)
+    ref[Packet.DESTINATION_PORT.index] = dst_port
 
 
 def __parse_ipv6_packet(data: bytes, ref: np.array) -> None:
@@ -115,10 +93,10 @@ def __parse_ipv6_packet(data: bytes, ref: np.array) -> None:
     """
     vtfl, payload_len, nxt_head, hop_lmt, src_ip, dst_ip = struct.unpack_from(bld_str_fmt([32, 16, 8, 8, 128, 128]), data)
     src_port, dst_port = parse_ports(data[40:])
-    ref[PKG_COLUMNS.index('SrcAddress')] = bytes_to_ip(src_ip)
-    ref[PKG_COLUMNS.index('SrcPort')] = src_port
-    ref[PKG_COLUMNS.index('DstAddress')] = bytes_to_ip(dst_ip)
-    ref[PKG_COLUMNS.index('DstPort')] = dst_port
+    ref[Packet.SOURCE_IP.index] = bytes_to_ip(src_ip)
+    ref[Packet.SOURCE_PORT.index] = src_port
+    ref[Packet.DESTINATION_IP.index] = bytes_to_ip(dst_ip)
+    ref[Packet.DESTINATION_PORT.index] = dst_port
 
 
 def __parse_arp_packet(data: bytes, ref: np.array) -> None:
@@ -140,28 +118,30 @@ def __parse_arp_packet(data: bytes, ref: np.array) -> None:
     hw_type, prot_type, hw_addr_len, prot_addr_len, op = struct.unpack_from(bld_str_fmt([16, 16, 8, 8, 16]), data[:9])
     packet_bits: List[int] = [hw_addr_len * 8, prot_addr_len * 8, hw_addr_len * 8, prot_addr_len * 8]
     send_mac, send_ip, rec_mac, rec_ip = struct.unpack_from(bld_str_fmt(packet_bits), data[8:])
-    ref[PKG_COLUMNS.index('ProtType')] = prot_type
-    ref[PKG_COLUMNS.index('Operation')] = op
-    ref[PKG_COLUMNS.index('SrcAddress')] = bytes_to_ip(send_ip)
-    ref[PKG_COLUMNS.index('DstAddress')] = bytes_to_ip(rec_ip)
+    ref[Packet.PROT_TYPE.index] = prot_type
+    ref[Packet.OPERATION.index] = op
+    ref[Packet.SOURCE_IP.index] = bytes_to_ip(send_ip)
+    ref[Packet.DESTINATION_IP.index] = bytes_to_ip(rec_ip)
 
 
 def __parse_vlan_packet(data: bytes, ref: np.array) -> None:
     pass
 
 
-def parse_packet(packet_data: bytes) -> np.array:
+def parse_packet(packet_data: bytes, ex_packet_data: List[Union[str, int, None]]) -> np.array:
+    """
+    Function parses bytes to python data and stores it in provided list.
+
+    :param packet_data: The bytes to be parsed
+    :param ex_packet_data: reference to list
+    """
     try:
-        ret_value: List[Union[int, str, None]] = [None for _ in range(12)]
-        __parse_ethernet_frame(packet_data, ret_value)
-        eth_type_str = [key.rsplit('_')[-1].lower() for key, value in socket_eth_types.items() if ret_value[1] == value][0]
-        globals()[f'__parse_{eth_type_str}_packet'](packet_data[14:], ret_value)
-        return ret_value
-    except struct.error:
-        # Error while parsing - ignore packet - continue
+        __parse_ethernet_frame(packet_data, ex_packet_data)
+        eth_type_str = [key.rsplit('_')[-1].lower() for key, value in socket_eth_types.items() if
+                        ex_packet_data[Packet.ETHERTYPE.index] == value][0]
+        globals()[f'__parse_{eth_type_str}_packet'](packet_data[14:], ex_packet_data)
+    except (struct.error, IndexError):
+        # IndexError occurs when EtherType not supported
+        # If a captured packet is too short to parse, Struct.error will be risen
+        # -> No concern - Proceed and continue with next packet!
         return None
-    except IndexError:
-        raise CaptureError(f'Ethernet-Type - {ret_value[1]} - not supported')
-
-
-
