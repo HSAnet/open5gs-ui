cdfa83d46ce429a691e21869dc7c45b558ac5fdb
Winkler-Jonas <jonas.eduard.winkler@gmail.com>
2024-01-05 21:32:47 +0100

    added open5gsexception

--- app/utils/open5g_rake/exceptions.py
+++ app/utils/open5g_rake/exceptions.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 class BashException(Exception):
     def __init__(self, errno: int, msg: str, cmd: str):
         self._msg: str = msg
@@ -15,3 +18,19 @@ class BashException(Exception):
     @property
     def errno(self) -> int:
         return self._errno
+
+
+# ------------------------------------------------------------------------------------------------------------------
+
+class Open5gsException(Exception):
+    def __init__(self, msg: str, prev_error: Union[BashException] = None):
+        self._msg: str = msg
+        self._prev_error: Union[BashException] = prev_error
+        super(Open5gsException, self).__init__('msg: {}'.format(msg))
+
+    def __reduce__(self):
+        return Open5gsException, self._msg
+
+    @property
+    def msg(self) -> str:
+        return f"{self._msg}{f'\nCaused by -> {self._prev_error.msg}' if self._prev_error else ''}"
