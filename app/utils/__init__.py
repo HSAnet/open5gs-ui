e2fdc4c2779b95b755acaf8b18032cd8f77a3808
Winkler-Jonas <jonas.eduard.winkler@gmail.com>
2024-01-06 20:54:22 +0100

    structure cleanup

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,5 +1,3 @@
 from .shared_memory import SharedMemory, Flags
-from .bash import Bash, BashCommands
 from .exceptions import BashException, CleanupException, ArgsException, ExecError, e_print
-from .parse_pattern import ParsePattern
 from .logger import LOG_QUEUE, worker_config, start_logger, stop_logger
--- app/utils/exceptions.py
+++ app/utils/exceptions.py
@@ -1,27 +1,6 @@
 import sys
 
 
-class BashException(Exception):
-    def __init__(self, errno: int, msg: str, cmd: str):
-        self._msg: str = msg
-        self._errno: int = errno
-        self.__cmd: str = cmd
-        super(BashException, self).__init__('msg: {}, errno: {}, cmd: {}'.format(msg, errno, cmd))
-
-    def __reduce__(self):
-        return BashException, (self._msg, self._errno, self.__cmd)
-
-    @property
-    def msg(self) -> str:
-        return f'Command: "{self.__cmd}" could not be executed! ErrNo: {self._errno} -> SysMessage: {self._msg}'
-
-    @property
-    def errno(self) -> int:
-        return self._errno
-
-
-# ---------------------------------------------------------------------------------------------------
-
 class CleanupException(Exception):
 
     def __init__(self, msg: str, err_code: int, caught: Exception = None):
