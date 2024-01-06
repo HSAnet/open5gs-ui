31801e9644a382b977f8e3fc4dd0a01115efad40
Winkler-Jonas <jonas.eduard.winkler@gmail.com>
2024-01-06 01:53:14 +0100

    Added log reading and json parser

--- app/utils/open5g_rake/rake.py
+++ app/utils/open5g_rake/rake.py
@@ -1,41 +1,57 @@
-from typing import List
+import datetime
+import json
+from typing import List, Union, Callable
 from pathlib import Path
 import regex as re
 
-from app.utils.open5g_rake.utils.service_parser import ParsePattern
-from app.utils.open5g_rake.exceptions import BashException
+from app.utils.open5g_rake.exceptions import BashException, Open5gsException
 from app.utils.open5g_rake.utils.bash import Bash, BashCommands
 from app.utils.open5g_rake.utils.service import Service
-
-
-
 
 
 class Open5GRake:
 
     def __init__(self, log_files_dir: Path = None):
-        self.__path: Path = log_files_dir
-        self.__service_list: List[Service]
+        self.__path: Union[None, Path] = log_files_dir
+        self.__service_list: List[Service] = []
 
         self.__get_service_list()
 
     def __get_service_list(self) -> None:
-        pattern = re.compile(r"(?P<service_name>open5gs-\w+\.service).*?(?<=Open5GS\s)(?P<log_name>[\w\-]+)", re.M)
+        """
+        Function makes executes bash-command 'systemctl list-units open5gs-* --all' and parses
+        result into Services which then are attached to self.__service_list.
+
+        :raises Open5GSException: If error occurs while executing bash-command
+        """
+        pattern = re.compile(r"(?P<service_name>open5gs-\w+\.service).*?(?<=Open5GS\s)(?P<log_name>[A-Z]{3,4}(?:-[A-Z])?)", re.M)
         try:
             result = Bash().run(BashCommands.OPENFIVEGSERVICES.value)
             for line in (l for l in result.split('\n') if l.strip().startswith('open5gs-')):
                 if match := pattern.search(line):
                     service_name = match.group('service_name')
                     log_name = match.group('log_name').lower().replace('-', '')
-                    print(f'Service_name: {service_name}\nLog_name: {log_name}')
-        except BashException:
-            # Todo: Kill process
-            print('Error occurred.. end program!')
+                    self.__service_list.append(Service(service_name, None if not self.__path else self.__path / log_name))
+        except BashException as be:
+            raise Open5gsException("Error occurred while trying to access service list.", prev_error=be)
 
-    def check_status(self):
+    @property
+    def service_list(self) -> List[Service]:
+        return self.__service_list
+
+    def rake(self, time_delta: int = None):
         for service in self.__service_list:
-            print(service.status)
+            for log in service.get_logs(time_delta):
+                yield log
+
+    def get_json(self, time_delta: int = None):
+        return f'{{\"services\": {{\"service": [{','.join([service.to_json(time_delta) for service in self.__service_list])}]}}}}'
 
 
 if __name__ == '__main__':
-    rake = Open5GRake()
+    rake = Open5GRake(Path('/var/log/open5gs/'))
+    # for service_log in rake.rake(10):
+    #     print(service_log)
+
+    print(rake.get_json(10))
+
--- app/utils/open5g_rake/utils/service.py
+++ app/utils/open5g_rake/utils/service.py
@@ -1,11 +1,14 @@
+import json
+
 import regex as re
-from datetime import datetime
-from typing import Union, Dict
+from datetime import datetime, timedelta
+from typing import Union, Dict, List, Callable
 from pathlib import Path
 
 from app.utils.open5g_rake.utils.bash import Bash, BashException, BashCommands
 
-status_pattern = re.compile(r'Active:\s(?P<status>\w+).*?(?<=since)[\D\s]*(?P<date>[\d\s\-:]+)(.*?Memory:\s(?P<memory>[\d.]+))?.*?CPU:\s(?P<cpu>\d+)', re.DOTALL)
+status_pattern = re.compile(
+    r'Active:\s(?P<status>\w+).*?(?<=since)[\D\s]*(?P<date>[\d\s\-:]+)(.*?Memory:\s(?P<memory>[\d.]+))?.*?CPU:\s(?P<cpu>\d+)', re.DOTALL)
 
 
 class Service:
@@ -13,13 +16,13 @@ class Service:
     def __init__(self, service_name: str, log_file: Union[None, Path]):
         self.__service_name: str = service_name
         self.__log_file: Path = None if not log_file else log_file.parent / (log_file.name + '.log')
-        print(f'Path of log-file: {self.__log_file}')
+        self.__file_access: bool = True
         self.__status: Union[None, Dict[str, Union[str, datetime, None, bool]]] = {
-                    'status': False,
-                    'up_date': None,
-                    'memory': None,
-                    'cpu': None
-                }
+            'status': False,
+            'since': None,
+            'memory': None,
+            'cpu': None
+        }
 
     @property
     def service_name(self) -> str:
@@ -34,6 +37,10 @@ class Service:
     def log_file(self) -> Union[None, Path]:
         return self.__log_file
 
+    @property
+    def file_access(self) -> bool:
+        return self.__file_access
+
     def __get_status(self) -> None:
         """
         Executes status check on service. Retrieves status, up-time, memory-usage and cpu-time.
@@ -44,23 +51,66 @@ class Service:
         try:
             result = Bash().run(BashCommands.CTL_STATUS.value.format(service_name=self.service_name))
             x = status_pattern.search(result)
-            print(x.groups())
             if match := status_pattern.search(result):
                 self.__status['status'] = True if 'active' == match.group('status') else False
-                self.__status['up_date'] = datetime.fromisoformat(match.group('date').strip())
+                self.__status['since'] = match.group('date').strip()
                 self.__status['memory'] = match.group('memory')
                 self.__status['cpu'] = match.group('cpu')
             else:
                 self.__status['status'] = False
         except BashException as be:
-            print(be.msg)
             self.__status['status'] = False
+
+    def get_logs(self, time_delta: Union[int, None]) -> List[Dict[str, Union[datetime, str]]]:
+        """
+        Access service-log data via file-reader or journalctl.
+        If provided path is not accessible or permission denied journalctl is executed.
+
+        :raises BashException: If journalctl command encountered error
+        :return: Log-String-Data (not parsed)
+        """
+        if self.__file_access:
+            try:
+                with self.log_file.open() as file:
+                    return self.__parse_log_data(file.read(), time_delta)
+            except (PermissionError, OSError, AttributeError) as e:
+                # User might not have permission to access the file
+                # OS might encounter error when opening the file
+                # The provided path is not valid or is None
+                # Disable file-reading and call function to access log data via journalctl
+                self.__file_access = False
+                return self.get_logs(time_delta=time_delta)
+        else:
+            # Raises BashException if error while executing command
+            return self.__parse_log_data(
+                Bash().run(BashCommands.CTLSERVICELOG.value.format(service_name=self.service_name)), time_delta)
+
+    def __parse_log_data(self, log_data: str, time_delta: Union[int, None]) -> List[Dict[str, Union[datetime, str]]]:
+        pattern = re.compile(r'(?P<date>^\d{2}/\d{2})\s(?P<time>[\d:]+).*?(?P<level>DEBUG|INFO|WARNING|CRITICAL):\s(?P<msg>.*)', re.MULTILINE)
+        is_new_log: Callable[[datetime], bool] = lambda lg_ts: True if not time_delta else (
+                lg_ts > (datetime.now() - timedelta(seconds=time_delta)))
+        return [{'date': log_date.strftime('%d.%m.%Y %H:%M:%S'),
+                 'level': match.group('level'),
+                 'msg': match.group('msg')
+                 } for line in log_data.splitlines() if (match := pattern.search(line)) and
+                is_new_log((log_date := datetime.fromisoformat(f'{datetime.now().year}' 
+                                                               f'{match.group('date').replace('/', '')} '
+                                                               f'{match.group('time')}')))]
+
+    def to_json(self, time_delta: Union[int, None]) -> str:
+        return (f"{{\"Name\": \"{self.service_name}\",\"Status\": \"{self.__status['status']}\",\""
+                f"{'Up' if self.status['status'] else 'Down'} "
+                f"since\": \"{self.__status['since']}\","
+                f"\"CPU usage\": \"{self.__status['cpu']}\","
+                f"\"Mem usage\": \"{'0' if not self.__status['memory'] else self.__status['memory']}\","
+                f"\"logs\": {json.dumps([log for log in self.get_logs(time_delta)])}"
+                f"}}")
 
     def __str__(self):
         self.__get_status()
         return (f"Service: {self.service_name}\n"
                 f"Log-File: {'Unknown' if not self.log_file else self.log_file}\n"
                 f"Status: {'Active' if self.__status['status'] else 'Inactive'}\n"
-                f"{'Up' if self.__status['status'] else 'Down'} since {self.__status['up_date'].strftime('%d.%m.%Y %H:%M:%S')} {int((datetime.now() - self.__status['up_date']).total_seconds() // 60)} Minutes\n"
+                f"{'Up' if self.__status['status'] else 'Down'} since {self.__status['since'].strftime('%d.%m.%Y %H:%M:%S')} {int((datetime.now() - self.__status['since']).total_seconds() // 60)} Minutes\n"
                 f"\tCPU usage: {self.__status['cpu']} ms\n"
                 f"\tMem usage: {'0' if not self.__status['memory'] else self.__status['memory']} MB")
