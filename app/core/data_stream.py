4dc8847b584802eed247f2d8dbb8f26654d01c76
Winkler-Jonas <jonas.eduard.winkler@gmail.com>
2024-01-07 08:43:06 +0100

    Corrected calculation error

--- app/core/data_stream.py
+++ app/core/data_stream.py
@@ -2,7 +2,6 @@ import time
 import logging
 import traceback
 from typing import List
-from multiprocessing import Manager
 
 from app.utils import py_pcap as pcap
 from app.utils.open5g_rake import Open5GRake, Open5gsException
@@ -23,42 +22,16 @@ class MonitoringAgent:
         self._log_rake: Open5GRake = Open5GRake()
         self._net_cap: pcap.pcap.Capture = pcap.pcap.capture(self._network_device, bpf_filter)
 
-    @property
-    def bpf_filter(self):
-        return self._bpf_filter
-
-    @property
-    def delay(self):
-        return self.delay
-
-    @property
-    def network_device(self):
-        return self.network_device
-
-    @property
-    def agent_logger(self):
-        return self.agent_logger
-
-    @property
-    def log_rake(self):
-        return self.log_rake
-
-    @property
-    def net_cap(self):
-        return self.net_cap
-
-    @staticmethod
-    def run(queue, logg):
-        self: MonitoringAgent = queue.get()
+    def run(self):
         while True:
             try:
                 start_time = time.time()
-                self.collect_logs(self.delay if 'elapsed_time' not in locals() else elapsed_time + self.delay)
-                self.capture_network_traffic()
-                self.send_data()
+                self._collect_logs(self._delay if 'elapsed_time' not in locals() else elapsed_time + self._delay)
+                self._capture_network_traffic()
+                self._send_data()
                 end_time = time.time()
-                if (elapsed_time := (end_time - start_time)) < self.delay:
-                    time.sleep(elapsed_time - self.delay)
+                if (elapsed_time := (end_time - start_time)) < self._delay:
+                    time.sleep(self._delay - elapsed_time)
             except pcap.pcap.NetworkError as ne:
                 print(ne.msg)
             except Open5gsException as oge:
@@ -68,15 +41,15 @@ class MonitoringAgent:
             except:
                 print(traceback.format_exc())
 
-    def collect_logs(self, delta: int):
-        print(self.log_rake.rake_json(time_delta=delta))
+    def _collect_logs(self, delta: int):
+        print(self._log_rake.rake_json(time_delta=delta))
 
-    def capture_network_traffic(self):
-        if self.net_cap.error():
-            raise pcap.pcap.NetworkError(self.net_cap.get())
+    def _capture_network_traffic(self):
+        if self._net_cap.error():
+            raise pcap.pcap.NetworkError(self._net_cap.get())
         else:
-            print(self.net_cap.get())
+            print(self._net_cap.get())
 
-    def send_data(self):
+    def _send_data(self):
         pass
 
