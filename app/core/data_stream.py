71e5ec6339a6c3a4f7e6d14ebf939adf1ecf7134
Winkler-Jonas <jonas.eduard.winkler@gmail.com>
2024-01-07 07:14:08 +0100

    integrated data-collector modules

--- app/core/data_stream.py
+++ app/core/data_stream.py
@@ -1,9 +1,10 @@
-import logging
 import time
+import logging
 import traceback
 from typing import List
 
 from ..utils import py_pcap as pcap
+from ..utils.open5g_rake import Open5GRake, Open5gsException
 
 
 class MonitoringAgent:
@@ -17,25 +18,37 @@ class MonitoringAgent:
         self._bpf_filter = bpf_filter
         self._delay = int(delay)
         self._network_device = network_device
-
+        self._agent_logger = logging.getLogger(__name__)
+        self._log_rake: Open5GRake = Open5GRake()
+        self._net_cap: pcap.pcap.Capture = pcap.pcap.capture(self._network_device, bpf_filter)
 
     def run(self):
-        self.__agent_logger = logging.getLogger(__name__)
         while True:
             try:
-                self._collect_logs()
+                start_time = time.time()
+                self._collect_logs(self._delay if 'elapsed_time' not in locals() else elapsed_time + self._delay)
                 self._capture_network_traffic()
                 self._send_data()
-                time.sleep(self._delay)
+                end_time = time.time()
+                if (elapsed_time := (end_time - start_time)) < self._delay:
+                    time.sleep(elapsed_time - self._delay)
+            except pcap.pcap.NetworkError as ne:
+                print(ne.msg)
+            except Open5gsException as oge:
+                print(oge.msg)
+            except KeyboardInterrupt:
+                break
             except:
-                # Todo proper error handling
                 print(traceback.format_exc())
 
-    def _collect_logs(self):
-        pass
+    def _collect_logs(self, delta: int):
+        print(self._log_rake.rake_json(time_delta=delta))
 
     def _capture_network_traffic(self):
-        pass
+        if self._net_cap.error():
+            raise pcap.pcap.NetworkError(self._net_cap.get())
+        else:
+            print(self._net_cap.get())
 
     def _send_data(self):
         pass
