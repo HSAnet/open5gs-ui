d9e70b887ef239862b2ec654d9bcc329386afe34
Winkler-Jonas <jonas.eduard.winkler@gmail.com>
2024-01-08 22:51:38 +0100

    added Server to packege imports

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -4,3 +4,4 @@ from .logger import LOG_QUEUE, worker_config, start_logger, stop_logger
 from .configurator import Config
 from .open5g_rake import *
 from .py_pcap import *
+from .server_connection import Server
