1d6cf59997ad412973ec6c61a8f1a928eecce87e
Jonas Winkler <jonas.winkler@hs-augsburg.de>
2024-01-09 23:39:41 +0100

    Simplyfied and reworked config file interaction

--- app/utils/configurator.py
+++ app/utils/configurator.py
@@ -1,107 +1,63 @@
-import regex as re
-from enum import Enum
-from typing import Dict, List, Union
 from pathlib import Path
+import regex as re
 
 
-class _Pattern(Enum):
-    DELAY = re.compile(r'^Delay[\s=]+(?P<delay>\d+)?', re.MULTILINE)
-    SERVER = re.compile(r'^Server[\s=]+(?P<server>.+)?', re.MULTILINE)
-    PORT = re.compile(r'^Port[\s=]+(?P<port>\d+)?', re.MULTILINE)
-    SERVER_KEY = re.compile(r'^Server_Key[\s=]+(?P<server_key>.+)?', re.MULTILINE)
-    SERVER_SNI = re.compile(r'^Server_SNI[\s=]+(?P<server_sni>.+)?', re.MULTILINE)
-    PUBLIC_KEY = re.compile(r'^Public_Key[\s=]+(?P<public_key>.+)?', re.MULTILINE)
-    PRIVATE_KEY = re.compile(r'^Private_Key[\s=]+(?P<private_key>.+)?', re.MULTILINE)
-    BPF = re.compile(r'^BPF-Filter[\s=]+(?P<bpf>.+)?', re.MULTILINE)
-    LOG_DIR = re.compile(r'^Dir[\s=]+(?P<log_dir>.+)?', re.MULTILINE)
-    DEVICE = re.compile(r'^Device[\s=]+(?P<device>.+)?', re.MULTILINE)
-
-    @classmethod
-    def parse(cls, string: str):
-        return {m[0]: m[1] for m in (pattern.in_string(string) for pattern in _Pattern) if m}
-
-    def in_string(self, string: str) -> Union[None, List[str]]:
-        if match := self.value.search(string):
-            return [(key := self.name.lower()), match.group(key)]
-
-
-class _Singleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(_Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
-
-class Config(metaclass=_Singleton):
-
-    _instance = None
+class Config:
 
     def __init__(self, config_file: Path):
+        """
+
+        .:raises AttributeError: if provided config-file does not exist / permission to access not granted or
+        is not readable          other File related OSErrors
+        :param config_file: Path to the config file
+        """
         if not (config_file.exists() and config_file.is_file()):
             raise AttributeError('Config file not found!')
         self._config_file = config_file
-        self._settings: Dict[str, str] = dict()
-
+        self._line_pattern = re.compile(r'^(?P<key>\w+)[\s=]{3}(?P<value>.*)', re.M)
         self._parse_config_file()
 
-    def _parse_config_file(self):
+    def _parse_config_file(self) -> None:
+        """
+        Method parses instance-level config-file and creates attributes
+        to access the config-file values like the following:
+
+            .conf [File]
+                Attr = Value
+            Config() [class]
+                Config().attr (returns Value)
+
+        :raises AttributeError: if config file could not be access due to permission or other OSErrors
+        """
         try:
             with self._config_file.open() as file:
-                for line in file:
-                    self._settings |= _Pattern.parse(line)
+                for line_nr, line in enumerate(file):
+                    if match := self._line_pattern.search(line):
+                        setattr(self, match.group('key').lower(), match.group('value'))
+                    elif not line.startswith('#'):
+                        # Todo: log this - Config line was not parsed
+                        print(f'[ConfigParser] - Error - Line {line_nr + 1}: "{line.strip()}" ignored!')
         except (PermissionError, OSError, AttributeError):
             raise AttributeError('Config file could not be accessed!')
 
-    @property
-    def bpf_filter(self):
-        return self._settings["bpf"]
+    def has(self, attribute: str) -> bool:
+        """
+        Check if config contains value you are looking for.
+        Either use this method or access it with try - except.
 
-    @property
-    def delay(self):
-        return self._settings["delay"]
+        :param attribute: The attribute to look for
+        :return: True if value is found, else False
+        """
+        if attribute in self.__dict__:
+            return True
 
-    @property
-    def server_key(self):
-        return self._settings["server_key"]
-
-    @property
-    def server_sni(self):
-        return self._settings["server_sni"]
-
-    @property
-    def public_key(self):
-        return self._settings["public_key"]
-
-    @property
-    def private_key(self):
-        return self._settings["private_key"]
-
-    @property
-    def log_dir(self):
-        return self._settings["log_dir"]
-
-    @property
-    def port(self):
-        return self._settings["port"]
-
-    @property
-    def server(self):
-        return self._settings["server"]
-
-    @property
-    def device(self):
-        return self._settings["device"]
-
-    # Neet Idea, but not really practical
-    #
-    # @staticmethod
-    # def _create_new_method(key: str):
-    #     def method(self):
-    #         return self._settings[key]
-    #     return method
-    #
-    # def _setup_methods(self):
-    #     for key in self._settings.keys():
-    #         setattr(self, f'get_{key}', MethodType(self._create_new_method(key), self))
+# if __name__ == '__main__':
+#     conf: Config = Config(Path('./settings.conf'))
+#     print(dir(conf))
+#     # Either access values like this
+#     print('BPF_Filter not found!' if not conf.has('bpf_filter') else conf.bpf_filter)
+#     # - or - like this
+#     try:
+#         print(conf.bpf_filter)
+#     except AttributeError:
+#         print('BPF_Filter not found!')
