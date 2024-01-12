16f5eecdfe442dba9f974d0c7a36fded77fe5903
Jonas Winkler <jonas.winkler@hs-augsburg.de>
2024-01-12 11:49:20 +0100

    Added scripts and config files to _static for displaying or downloading

--- docs/source/_static/open5gs_system_setup.sh
+++ docs/source/_static/open5gs_system_setup.sh
@@ -24,3 +24,8 @@ systemctl status open5gs-webui.service | grep 'Ready on'
 sudo touch /etc/init.d/open5g_startup.sh
 sudo bash -c "echo -e 'sysctl -w net.ipv4.ip_forward=1; sysctl -w net.ipv6.conf.all.forwarding=1; iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE; ip6tables -t nat -A POSTROUTING -s 2001:db8:cafe::/48 ! -o ogstun -j MASQUERADE; ufw disable; iptables -I INPUT -i ogstun -j ACCEPT; iptables -I INPUT -s 10.45.0.0/16 -j DROP; ip6tables -I INPUT -s 2001:db8:cafe::/48 -j DROP' > /etc/init.d/open5g_startup.sh"
 sudo chmod 700 /etc/init.d/open5g_startup.sh
+
+# Download configuration files
+sudo rm /etc/open5gs/upf.yaml /etc/open5gs/amf.yaml
+sudo wget -P /etc/open5gs/ https://download-link
+sudo wget -P /etc/open5gs/ https://download-link
---
+++ docs/source/_static/ueransim_system_setup.sh
@@ -0,0 +1,34 @@
+CMAKE_VERSION="3.27.9"
+
+# Setup System
+sudo apt update
+sudo apt upgrade
+sudo apt install make gcc g++ libsctp-dev lksctp-tools iproute2 libssl-dev git -y
+
+# Install cmake (APT version is too old!)
+function install_cmake () {
+  wget -qO- https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.tar.gz | tar xvz -C ~/Downloads/
+  cd ~/Downloads/cmake-$CMAKE_VERSION/ || return
+  ./configure --prefix=/opt/cmake
+  gmake -j4
+  sudo make -j4 install
+  rm -rf ~/Downloads/cmake-$CMAKE_VERSION
+}
+install_cmake
+
+
+# Export CMAKE-Path to environment
+sudo echo "$(echo "PATH=\"")""$(cat /etc/environment|grep -oP 'PATH="\K[^"]+')""$(echo ":/opt/cmake/bin")""$(echo "\"")" > /etc/environment
+export PATH=$PATH:/opt/cmake/bin
+
+# Install Uransim
+git clone https://github.com/aligungr/UERANSIM ~/UERANSIM
+~/UERANSIM/make -j4
+
+# create alias to startup ueransim with configuration
+echo -e "function start_ueransim() {\n\t~/UERANSIM/build/nr-gnb -c ~/UERANSIM/config/gnb1.yaml\n\t~/UERANSIM/build/nr-ue -c ~/UERANSIM/config/ue1.yaml\n\tsudo ip r add default dev uesimtun0\n}" >> ~/.bashrc
+# A network application can be executed like this
+# ~/UERANSIM/build/nr-binder 10.45.0.3 firefox
+
+# Restart System
+sudo systemctl reboot
